b745f1f1dda847b86e0de7933e282da5ebd00b1d
Replit user <>
2023-10-22 12:47:00 +0000

    Prima parte di alcuni compiti per casa

--- l4/es1.py
+++ l4/es1.py
@@ -1,12 +1,61 @@
+# Primo esercizio della lezione di TRMD del 20 ottobre 2023.
+# Creare due array (ad esempio con numpy.arange()).
+# Inserire almeno 10 zeri nel secondo array.
+# L'obiettivo è quello di computare il prodotto tra il primo e il secondo array.
+# Bisogna farlo in tre modi:
+#  I. identificando gli zeri del secondo array e computando solo le entrate non nulle;
+#  II. sostituendo gli zeri del secondo array con 0.001 e computando il tutto;
+#  III. ignorando gli zeri del secondo array e computando il tutto.
+# Nel caso III. vanno identificati gli inf con numpy.isinf().
+
+# Importo numpy.
+
 import numpy as np
 
+# Creo i due array.
 
-array_1 = np.arange(0, 5, 0.5)
-array_2 = np.arange(0, 3, 0.3)
-result = array_2 - array_2
+a = np.arange(0, 10, 0.5)
+b = np.arange(0, 16, 0.8)
+na, = a.shape
+nb, = b.shape
+print("Primo array: \n {} \n Dimensione: \n {}" .format(a, na))
+print("Secondo array: \n {} \n Dimensione: \n {}"  .format(b, nb))
 
-zeros = np.where(array_2 != 0)[0]
-result = array_1[zeros]/array_2[zeros]
-print(result)
+# Inserisco gli zeri nel secondo array.
 
-array_2 = np.where(array_2 == 0, )
+bz = b
+for i in range(0, 20, 2):
+  bz[i] = 0
+print("Secondo array con zeri: \n {}" .format(bz))
+
+# I: identifico gli zeri del secondo array. Computo solo le entrate non nulle.
+
+z, = np.where(bz != 0)
+c1 = a[z]/bz[z] 
+nc1, = c1.shape
+print("Quoziente del primo tipo: \n {} \n Dimensione \n {}" .format(c1, nc1))
+
+# II: sostituisco gli zeri del secondo array. Computo tutto.
+
+bqz = np.where(b == 0, 0.001 , bz)
+nbqz, = bqz.shape
+print("Secondo array con (quasi) zeri: \n {} \n Dimensione: \n {}" .format(bqz, nbqz))
+c2 = a/bqz
+nc2, = c2.shape
+print("Quoziente del secondo tipo: \n {} \n Dimensione \n {}" .format(c2, nc2))
+
+# III: eseguo la divisione tra gli array originali e cerco gli infiniti.
+
+c3 = a/b
+nc3, = c3.shape
+print("Quoziente del terzo tipo: \n {} \n Dimensione: \n {}" .format(c3, nc3))
+inf = np.isinf(c3)
+ninf = []
+for i in range(nc3):
+  if inf[i]:
+    ninf = ninf + [i]
+  else:
+    continue
+print("Posizione degli infiniti nel quoziente: \n {}" .format(ninf))
+
+# Esercizio completato!
--- l4/es2.py
+++ l4/es2.py
@@ -1,5 +1,97 @@
+# Esercizio 2 della lezione del 20 ottobre 2023 di TRMD.
+# Utilizzare i dati contenuti in es2.dat per creare un grafico.
+# Le colonne utili sono "M_ass", 'b-y', 'age_parent[Gyr]'.
+# L'esercizio è diviso in due parti:
+#  I. creare uno scatter in cui tutti i punti abbiano colori diversi;
+#  II. creare uno scatter con varie classi di colori.
+
+# ===================================================
+
+# Importo i pacchetti e le librerie necessarie.
+
 import numpy as np
+import matplotlib.pyplot as mpl
 
-array_1 = np.arange(0, 3, 0.3)
-array_2 = np.arange(0, 5, 0.5)
+# Importo i dati in tre array.
 
+file = 'es2.dat'
+data = np.loadtxt(file, delimiter = ' ', usecols = (4, 8, 12), unpack = True)
+m = data[0]
+b = data[1]
+a = data[2]
+
+# Posso passare al punto I.
+
+# =========================
+
+# I. creo uno scatter in cui tutti i punti abbiano colori diversi.
+# Plotto il mio grafico tramite scatter usando colori diversi.
+
+mpl.scatter(b, m, c = a, cmap = 'winter_r', marker = '.', s = 5)
+k = mpl.scatter(b, m, c = a, cmap = 'winter_r', marker = '.', s = 5)
+
+# Imposto l'intervallo di visualizzazione e dò un nome agli assi.
+
+mpl.xlim(-0.1, 1)
+mpl.ylim(8.5, -4)
+
+mpl.xlabel('b-y')
+mpl.ylabel('M_V')
+
+# Aggiungo una colorbar con il comando colorbar e aggiungo un label.
+# La scala dei colori è data da 'age_parent'.
+
+mpl.colorbar(k, label = 'Gyr')
+
+# Salvo la figura ottenuta in questo modo. Pulisco il canvas.
+
+mpl.savefig('es2I.png')
+mpl.clf()
+
+# Posso passare al punto II.
+
+# ========================== 
+
+
+# II. Creo uno scatter con varie classi di colori
+# Devo usare l'array 'data' per associare a ciascuna coppia (m, b) il colore giusto.
+# Cerco minimo e massimo del vettore 'age_parent' (data[2]).
+
+mn = min(data[2])
+mx = max(data[2])
+
+# Seleziono il numero di classi e creo due vettori contenti gli estremi inf/sup.
+
+nc = 3
+d = (mx - mn)/nc
+inf = []
+sup = []
+for i in range(nc):
+  inf = inf + [i*d]
+  sup = sup + [(i + 1)*d]
+
+# Plotto un grafico per ciascuna classe. 
+
+for i in range(nc):
+  for j in range(data[2].shape[0]):
+    aj = data[2][j]
+    if aj >= inf[i] and aj <= sup[i]:
+      mpl.scatter(b, m, marker = '.', s = 5, label = ('{} Gyr - {}                                                          Gyr'.format(inf[i], sup[i])))
+    else:
+      continue
+
+# Imposto l'intervallo di visualizzazione e dò un nome agli assi. Creo una legenda.
+
+mpl.xlim(-0.1, 1)
+mpl.ylim(8.5, -4)
+
+mpl.xlabel('b-y')
+mpl.ylabel('M_V')
+
+mpl.legend()
+
+# Salvo la figura ottenuta in questo modo.
+
+mpl.savefig('es2II.png')
+
+# Esercizio completato.
